f20295cc27acc627ef90b78340dfb22bb14ff2a7
seifediine <seifeddine.elkorichi@hotmail.com>
2020-12-09 09:50:27 +0100

    final commit

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 // Init Github
 const github = new Github();
 
+// Init UI
+const ui = new UI();
+
 // Search Input
 const searchUser = document.getElementById("searchUser");
 
@@ -12,7 +15,17 @@ searchUser.addEventListener("keyup", (e) => {
   if (userText !== "") {
     // Make http call
     github.getUser(userText).then((data) => {
-      console.log(data);
+      if (data.profile.message === "Not Found") {
+        // Show Alert
+        ui.showAlert("User Not Found !", "alert alert-danger");
+      } else {
+        // Show Profile
+        ui.showProfile(data.profile);
+        ui.showRepos(data.repos);
+      }
     });
+  } else {
+    // Clear Profile
+    ui.clearProfile();
   }
 });
